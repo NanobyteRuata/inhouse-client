8259412fbe2d3d29f811c5166e980c7a29adc783
Remruat Thanga <remruatthanga.pro@gmail.com>
2021-06-11 15:35:07 +0630

    fix some bugs

--- src/app/constants/api-constants.ts
+++ src/app/constants/api-constants.ts
@@ -1,4 +1,4 @@
 export class ApiConstants {
-  static HOST_URL: string = 'http://localhost:8000/';
-  static BASE_URL: string = ApiConstants.HOST_URL + 'api/';
+  static HOST_URL: string = 'http://localhost:8000';
+  static BASE_URL: string = ApiConstants.HOST_URL + '/api/';
 }
--- src/app/page/overtime-page/overtime-page.component.ts
+++ src/app/page/overtime-page/overtime-page.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { EmployeeSearchComponentComponent } from 'src/app/component/employee-search-component/employee-search-component.component';
+import { Employee } from 'src/app/model/employee-model';
+import { EncryptionUtil } from 'src/app/util/encryption-util';
 
 @Component({
   selector: 'app-overtime-page',
@@ -6,11 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./overtime-page.component.scss'],
 })
 export class OvertimePageComponent implements OnInit {
-  selectedEmployee: any = null;
+  @ViewChild('employeeSearchComponent')
+  employeeSearchComponent1!: EmployeeSearchComponentComponent;
+
+  currentUserEmployee: Employee;
+  selectedEmployee: Employee;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initializeCurrentUser();
+    let waitComponentToLoadInterval = setInterval(() => {
+      if (this.employeeSearchComponent1 != null) {
+        // set current employee in employeeSearch component on left panel
+        this.employeeSearchComponent1.employees = [this.currentUserEmployee];
+        this.employeeSearchComponent1.selectedEmployee =
+          this.currentUserEmployee;
+
+        this.onSelectEmployee(this.currentUserEmployee);
+        clearInterval(waitComponentToLoadInterval);
+      }
+    }, 500);
+  }
+
+  initializeCurrentUser(): void {
+    let currentUserDataJsonString = localStorage.getItem('current_employee');
+    let decryptedUserData = EncryptionUtil.decryptData(
+      currentUserDataJsonString
+    );
+    this.currentUserEmployee = decryptedUserData;
+  }
 
   onSelectEmployee(employee: any) {
     this.selectedEmployee = employee;
