cafb2147c76014b1c4ff0851b6d71aad37753adc
Remruat Thanga <remruatthanga.pro@gmail.com>
2021-06-18 14:20:32 +0630

    new build

--- src/app/constants/api-constants.ts
+++ src/app/constants/api-constants.ts
@@ -1,4 +1,4 @@
 export class ApiConstants {
-  static HOST_URL: string = 'http://128.199.144.229/';
+  static HOST_URL: string = 'http://128.199.144.229';
   static BASE_URL: string = ApiConstants.HOST_URL + '/api/';
 }
