f7d60a13557ad28727b8f0c9192b34ebee28a1f4
Rem Ruat Thanga <remruatthanga.mysol@gmail.com>
2023-03-22 11:16:52 +0630

    config for localhost

--- src/app/constants/api-constants.ts
+++ src/app/constants/api-constants.ts
@@ -1,4 +1,4 @@
 export class ApiConstants {
-  static HOST_URL: string = 'http://128.199.144.229';
+  static HOST_URL: string = 'http://127.0.0.1:8000';
   static BASE_URL: string = ApiConstants.HOST_URL + '/api/';
 }
